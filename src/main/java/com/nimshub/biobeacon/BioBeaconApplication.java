feea5bfd8e0c9bbb7a772210d4ec78ac1fe89139
nirmala <nirmalasabaragamuwa@gmail.com>
2023-02-11 23:48:57 +0530

    init

---
+++ src/main/java/com/nimshub/biobeacon/BioBeaconApplication.java
@@ -0,0 +1,13 @@
+package com.nimshub.biobeacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioBeaconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioBeaconApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nimshub/biobeacon/BioBeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimshub.biobeacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioBeaconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
