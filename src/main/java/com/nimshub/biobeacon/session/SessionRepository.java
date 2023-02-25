c81080d5dd4c74bfa410b8a3b18f9ea8b615634b
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-02-26 03:54:23 +0530

    updated SessionRepository

--- src/main/java/com/nimshub/biobeacon/session/SessionRepository.java
+++ src/main/java/com/nimshub/biobeacon/session/SessionRepository.java
@@ -2,5 +2,5 @@ package com.nimshub.biobeacon.session;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SessionRepository extends JpaRepository {
+public interface SessionRepository extends JpaRepository<Session,Integer> {
 }
