339ca72eef77c1131097be84fd67a242c7d445b5
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-03-02 02:45:59 +0530

    refactoring

--- src/main/java/com/nimshub/biobeacon/user/UserController.java
+++ src/main/java/com/nimshub/biobeacon/user/UserController.java
@@ -1,7 +1,10 @@
 package com.nimshub.biobeacon.user;
 
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.internal.util.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,12 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
     Logger logger = LoggerFactory.getLogger(UserController.class);
     @GetMapping()
     @PreAuthorize("hasAuthority('ADMIN')")
