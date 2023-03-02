ac5222e80e7391a0a09d03d316bb6078626dd1aa
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-03-03 04:07:14 +0530

    test updated

--- src/test/java/com/nimshub/biobeacon/BioBeaconApplicationTests.java
+++ src/test/java/com/nimshub/biobeacon/BioBeaconApplicationTests.java
@@ -1,13 +1,10 @@
 package com.nimshub.biobeacon;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BioBeaconApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
