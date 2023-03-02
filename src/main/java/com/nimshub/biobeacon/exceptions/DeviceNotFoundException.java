caf1abcab75405d05f7ddd974b11d020661a513b
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-03-03 03:21:17 +0530

    added exception handling with proper status codes

--- src/main/java/com/nimshub/biobeacon/exceptions/DeviceNotFoundException.java
+++ src/main/java/com/nimshub/biobeacon/exceptions/DeviceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.nimshub.biobeacon.exceptions;
 
-public class DeviceNotFoundException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DeviceNotFoundException extends RuntimeException {
     public DeviceNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/nimshub/biobeacon/exceptions/SessionNotFoundException.java
+++ src/main/java/com/nimshub/biobeacon/exceptions/SessionNotFoundException.java
@@ -1,5 +1,9 @@
 package com.nimshub.biobeacon.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class SessionNotFoundException extends RuntimeException{
     public SessionNotFoundException(String message) {
         super(message);
