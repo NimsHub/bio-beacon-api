22ab826174e9aac251038516d7bf297036d025fd
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-03-03 03:25:25 +0530

    parameter injection

--- src/main/java/com/nimshub/biobeacon/config/JwtService.java
+++ src/main/java/com/nimshub/biobeacon/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY="357638792F423F4528482B4D6251655468576D597133743677397A2443264629";
+    @Value("${secret.key}")
+    private String SECRET_KEY;
 
     /**
      *
