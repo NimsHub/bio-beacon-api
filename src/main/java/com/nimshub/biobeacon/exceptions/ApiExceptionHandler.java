918be27a733f1a6e746fccff7178e3d9c67bad35
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-03-07 14:20:07 +0530

    added exception handlers

--- src/main/java/com/nimshub/biobeacon/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/nimshub/biobeacon/exceptions/ApiExceptionHandler.java
@@ -1,21 +1,40 @@
 package com.nimshub.biobeacon.exceptions;
 
-import lombok.extern.slf4j.Slf4j;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-@Slf4j
 public class ApiExceptionHandler {
     @ExceptionHandler(DeviceNotFoundException.class)
-    public ResponseEntity<Object> handleDeviceNotFoundException(DeviceNotFoundException e){
-        return new  ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
-    }
-    @ExceptionHandler(SessionNotFoundException.class)
-    public ResponseEntity<Object> handleSessionNotFoundException(SessionNotFoundException e){
-        return new  ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> handleDeviceNotFoundException(DeviceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    @ExceptionHandler(SessionNotFoundException.class)
+    public ResponseEntity<Object> handleSessionNotFoundException(SessionNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AthleteNotFoundException.class)
+    public ResponseEntity<Object> handleAthleteNotFoundException(AthleteNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<Object> handleExpiredJwtException(ExpiredJwtException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
