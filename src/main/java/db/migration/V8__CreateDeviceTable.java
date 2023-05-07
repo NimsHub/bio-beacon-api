008d9d2e62e41b6669171d84f75cac9dc963a069
nirmala <nirmalasabaragamuwa@gmail.com>
2023-05-07 18:35:50 +0530

    added flyway script to create device table

---
+++ src/main/java/db/migration/V8__CreateDeviceTable.java
@@ -0,0 +1,33 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Database migration for creating Device table
+ */
+public class V8__CreateDeviceTable extends BaseJavaMigration {
+
+    @Override
+    public void migrate(Context context) throws Exception {
+        createDeviceTable(context);
+    }
+
+    private void createDeviceTable(Context context) throws SQLException {
+        try (PreparedStatement statement =
+                     context
+                             .getConnection()
+                             .prepareStatement(
+                                     "create table if not exists device(" +
+                                             " id bigint not null" +
+                                             " primary key," +
+                                             " api_key varchar(255)," +
+                                             " created_at timestamp" +
+                                             ");")) {
+            statement.execute();
+        }
+    }
+}
