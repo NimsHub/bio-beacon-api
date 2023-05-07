b1b1d60d958b8ef556b03bdb6662470d1ae488cb
nirmala <nirmalasabaragamuwa@gmail.com>
2023-05-07 17:15:36 +0530

    changed development database

--- src/main/java/db/migration/V1__Baseline.java
+++ src/main/java/db/migration/V1__Baseline.java
@@ -3,7 +3,11 @@ package db.migration;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class V1__Baseline extends BaseJavaMigration {
+    @Override
     public void migrate(Context context) throws Exception {
 
     }
