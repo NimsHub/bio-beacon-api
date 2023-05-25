cecd024f9005cf54a62b2ae4a9d1e4fadaa0c7bf
nirmala <nirmalasabaragamuwa@gmail.com>
2023-05-25 19:32:45 +0530

    suppress warnings

--- src/main/java/db/migration/V5__CreateCoachTable.java
+++ src/main/java/db/migration/V5__CreateCoachTable.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 /**
  * Database migration for creating Coach Table
  */
-//@SuppressWarnings({"java:S101", "unused"})
+@SuppressWarnings({"java:S101", "unused"})
 public class V5__CreateCoachTable extends BaseJavaMigration {
     @Override
     public void migrate(Context context) throws Exception {
