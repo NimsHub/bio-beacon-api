c5178943a958241aa9bebffbed1d9de7aff59a53
nirmala <nirmalasabaragamuwa@gmail.com>
2023-06-12 14:19:32 +0530

    configured scheduler

---
+++ src/main/java/com/nimshub/biobeacon/scheduler/Task.java
@@ -0,0 +1,16 @@
+package com.nimshub.biobeacon.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Task {
+//    @Scheduled(fixedRate = 1000)
+//    @Async
+    void testRun() throws InterruptedException {
+
+    }
+}
