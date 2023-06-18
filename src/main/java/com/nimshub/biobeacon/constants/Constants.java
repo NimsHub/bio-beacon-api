21aad44ebf749e13d55be1fc579dec451ea88b36
nirmala <nirmalasabaragamuwa@gmail.com>
2023-06-18 20:31:55 +0530

    created constants class

---
+++ src/main/java/com/nimshub/biobeacon/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.nimshub.biobeacon.constants;
+
+public class Constants {
+    // creating a private constructor to hide the public implicit one
+    private Constants() {
+        throw new IllegalStateException("Constants class");
+    }
+    public static final String NO_DATA = "*";
+    public static final String COMMA = ",";
+}
