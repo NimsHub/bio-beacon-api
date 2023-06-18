9c341349f484c5d50744d3f722b84b061430cbcb
nirmala <nirmalasabaragamuwa@gmail.com>
2023-06-18 23:51:29 +0530

    refactored BitReader

--- src/main/java/com/nimshub/biobeacon/utils/BitReader.java
+++ src/main/java/com/nimshub/biobeacon/utils/BitReader.java
@@ -1,18 +1,23 @@
 package com.nimshub.biobeacon.utils;
 
 import com.nimshub.biobeacon.exceptions.ByteCodeException;
+import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.StringJoiner;
 
+import static com.nimshub.biobeacon.constants.Constants.*;
+
+@Service
 public class BitReader {
     private Acceleration getAcceleration(byte[] accelerationBytes) {
 
         boolean isDataAvailable = ((accelerationBytes[0] & 0x80) >> 7) == 0;
 
         if (isDataAvailable)
-            return new Acceleration("*", "*", "*");
+            return new Acceleration(NO_DATA);
 
-        int z = ((accelerationBytes[2] & 0x0B) << 8) | (accelerationBytes[3] & 0xFF);
+        int z = ((accelerationBytes[2] & 0x03) << 8) | (accelerationBytes[3] & 0xFF);
         int y = ((accelerationBytes[1] & 0x0F) << 6) | ((accelerationBytes[2] & 0xFC) >> 2);
         int x = ((accelerationBytes[0] & 0x3F) << 4) | ((accelerationBytes[1] & 0xF0) >> 4);
 
@@ -20,11 +25,11 @@ public class BitReader {
     }
 
 
-    public String getMotionData(byte[] data) {
+    private String createMotionData(byte[] data) {
 
         if (data.length % 4 != 0) throw new ByteCodeException("Data error: missing or redundant data");
 
-        StringJoiner csv = new StringJoiner(",");
+        StringJoiner csv = new StringJoiner(COMMA);
 
         for (int i = 0; i < data.length; i += 4) {
             byte[] accelerationBytes = {
@@ -41,5 +46,12 @@ public class BitReader {
             csv.add(acceleration.getZ());
         }
         return csv.toString();
+    }
+
+    public String getMotionData(String motionData) {
+        return createMotionData(
+                Base64
+                        .getDecoder()
+                        .decode(motionData));
     }
 }
